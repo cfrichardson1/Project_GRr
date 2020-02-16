3d3c6d2d4c7746102674b316c0ac48a26c215034
cfrichardson1 <cfrichardson1@gmail.com>
2020-02-15 21:53:55 -0800

    rebuilding in demo_polish to fix bugs

--- app/routes.py
+++ app/routes.py
@@ -5,12 +5,8 @@ from flask import redirect, render_template, url_for
 
 @app.route('/index.html')
 def home():
-	return render_template('homepage.html', title_bar ='Circuit Syndicate Racing')
+	return render_template('site_homepage.html', title_bar ='Circuit Syndicate')
 
 @app.route('/product.html')
 def product():
-	return render_template('product.html', title_bar ='Circuit Syndicate Racing')
-
-@app.route('/about.html')
-def about():
-	return render_template('about.html', title_bar ='Circuit Syndicate Racing')
+	return render_template('site_product.html', title_bar ='Circuit Syndicate')
